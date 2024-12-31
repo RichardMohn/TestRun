8881343d055c9b0bf8ef99e6716fee1103cd35f5
RichardMohn <192837959+RichardMohn@users.noreply.github.com>
2024-12-30 20:15:22 -0500

    updates

--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -1,29 +1,8 @@
-using UnityEngine;
-
-public class BlockManager : MonoBehaviour
+void SpawnBlock()
 {
-    public GameObject blockPrefab;
-    public float spawnInterval = 2f;
-    public float blockSpeed = 2f;
-
-    private float timer;
-
-    void Update()
-    {
-        timer += Time.deltaTime;
-
-        if (timer >= spawnInterval)
-        {
-            SpawnBlock();
-            timer = 0f;
-        }
-    }
-
-    void SpawnBlock()
-    {
-        float randomX = Random.Range(-2f, 2f);
-        Vector3 spawnPosition = new Vector3(randomX, transform.position.y, 0);
-        GameObject block = Instantiate(blockPrefab, spawnPosition, Quaternion.identity);
-        block.GetComponent<BlockMovement>().speed = blockSpeed;
-    }
+    float randomX = Random.Range(-2f, 2f); // Random horizontal position
+    float randomY = Random.Range(1f, 3f); // Random vertical spacing between blocks
+    Vector3 spawnPosition = new Vector3(randomX, transform.position.y + randomY, 0);
+    GameObject block = Instantiate(blockPrefab, spawnPosition, Quaternion.identity);
+    block.GetComponent<BlockMovement>().speed = blockSpeed;
 }
