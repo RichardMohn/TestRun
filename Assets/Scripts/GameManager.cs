e355c8c26fec1af6cf4215c78b69d590bb7c4235
RichardMohn <192837959+RichardMohn@users.noreply.github.com>
2024-12-30 21:42:40 -0500

    Updates

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,29 +2,47 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int lives = 3;
-    public GameObject gameOverUI;
+    public static GameManager instance; // Singleton instance of GameManager
+    public GameObject gameOverUI; // Reference to the GameOver UI panel
 
-    public void LoseLife()
+    private void Awake()
     {
-        lives--;
-        if (lives <= 0)
+        // Ensure a single instance of GameManager
+        if (instance == null)
         {
-            GameOver();
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 
-    void GameOver()
+    public void GameOver()
     {
+        // Pause the game
         Time.timeScale = 0;
-        gameOverUI.SetActive(true);
+        // Activate the GameOver UI
+        if (gameOverUI != null)
+        {
+            gameOverUI.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("GameOver UI is not assigned in the GameManager!");
+        }
     }
 
     public void RestartGame()
     {
+        // Resume the game
         Time.timeScale = 1;
-        lives = 3;
-        gameOverUI.SetActive(false);
-        // Add scene reload or reset logic here
+        // Deactivate the GameOver UI
+        if (gameOverUI != null)
+        {
+            gameOverUI.SetActive(false);
+        }
+        // Reload the current scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
 }
